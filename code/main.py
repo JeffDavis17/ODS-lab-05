1895f4ee00bc8c3b52f901ea471696ba7a50470a
JeffDavis17 <47422520+JeffDavis17@users.noreply.github.com>
2020-03-17 13:05:09 -0400

    done?

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from scipy.linalg import block_diag
+from numpy.linalg import inv
+
 
 # Data
 data = pd.read_csv(r'C:/Users/jcdav/Documents/GitHub/ODS-lab-05/code/The data for #5.csv')
@@ -12,8 +14,8 @@ t = np.array(data[1:48,1],dtype=str)
 
 # Misclosure Matrix and Covariance
 w=[]
-count = 0
 cov = np.array([])
+lines = []
 
 for i in range (47):
     base_i = d[i,0:3]
@@ -27,32 +29,45 @@ for i in range (47):
             w.append(base_i - base_j)  
             cov_val = cov_i + cov_j
             cov = block_diag(cov,cov_val)
+            lines.append([f_val,t_val,'repeated forward'])
         elif f_val == t[i] and t_val == f[i]:
             w.append(base_i + base_j)
             cov_val = cov_i + cov_j
             cov = block_diag(cov,cov_val)
-            
+            lines.append([f_val,t_val,'forward/reverse'])       
+
 cov = np.delete(cov, 0, 0)
 w = np.ravel(w)
+lines = np.array(lines)
 
 # A priori estimation
-apr = (w.T@cov@w)/w.size
+apr = (w.T@inv(cov)@w)/w.size
 n_cov = apr*cov
 
 
 # 95% Confidence interval misclosure test:
 variance = np.diag(n_cov)
-compare = np.less_equal(w,1.96*variance) # Test if values are less than 196σ
+compare = np.less_equal(w,1.96*variance**(1/2)) # Test if values are less than 196σ
 compare = np.reshape(compare,[22,3])
 
 
 val = [];
 # Chi squared test
-for i in range(0,62,3):
+for i in range(0,66,3):
     w_i = np.array([w[i], w[i+1], w[i+2]])
     cov_i = n_cov[i:i+3,i:i+3]
-    val.append(w_i.T@cov_i@w_i)
+    val.append(w_i.T@inv(cov_i)@w_i)
 val = np.ravel(val)
-compare1 = np.less_equal(val,1.82)
+compare1 = np.less_equal(val,7.82)
 
 
+# Possible Outliers 
+compare2 = compare[:,0]*compare[:,1]*compare[:,2]
+count = np.arange(22)
+pos1 = count[compare1<1]
+pos2 = count[compare2<1]
+pos = []
+pos.append(lines[pos1[:],0:2])
+pos.append(lines[pos2[:],0:2])
+pos = np.array(pos)
+print(pos)
